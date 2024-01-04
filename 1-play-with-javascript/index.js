58167d72955135475da1352ae3243d038ba6639f
nag <nagtraininglab@gmail.com>
2024-01-04 13:29:17 +0530

    day-2 commit

--- 1-play-with-javascript/index.js
+++ 1-play-with-javascript/index.js
@@ -1,22 +1,2 @@
 
-// Pattern : module pattern
-
-// self-executable function
-// IIFE - Immediately Invoked Function Expression
-
-// e.g : counter module
-
-const counter = (function (initialCount = 0) {
-    let count = initialCount; // private
-    function increment() { // public
-        count++;
-    }
-    function getCount() { // public
-        return count;
-    }
-    return {
-        increment: increment,
-        getCount: getCount
-    }
-})(100)
 
