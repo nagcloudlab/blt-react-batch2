7398382ac177989716f67995cfe2fb2e4146b2f9
nag <nagtraininglab@gmail.com>
2024-01-05 10:05:01 +0530

    links

--- 1-play-with-javascript/index.js
+++ 1-play-with-javascript/index.js
@@ -1,2 +1,23 @@
 
 
+
+// Q
+
+let myFunctions = [];
+
+//---------------------------------------------
+
+function getF(i) {
+    let func = function () {
+        console.log(i);
+    }
+    return func;
+}
+
+for (var i = 0; i < 2; i++) {
+    myFunctions.push(getF(i));
+}
+//---------------------------------------------
+
+myFunctions[0]();
+myFunctions[1]();
